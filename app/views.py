28364d39270543ed9347b2aa5aecf49ea336bcac
peyton-schmidt <pschmidt25@basecampcodingacademy.org>
2024-10-23 10:07:07 -0500

    works

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+
+# Create your views here.
+
+def hello_view(request: HttpRequest):
+    return HttpResponse("Hello World")
